00993db8febc46ae6eaa92f3b0e9597b2f00e30a
keenmukund <mukundzplace@gmail.com>
2020-04-20 17:14:49 +0530

    Strings and Arrays

---
+++ Java Basics/New_String.java
@@ -0,0 +1,14 @@
+
+public class New_String {
+
+	public static void main(String[] args) {
+		char c[]= {'a','b','c'};
+		String str= new String(c);
+		String str1="Welcome to java";
+		 System.out.println(str);
+		 System.out.println(str1);
+		 
+		
+	}
+
+}
---
+++ Java Basics/String_Demo.java
@@ -0,0 +1,13 @@
+public class String_Demo {
+
+	public static void main(String[] args) {
+		String s1="Happy";
+		System.out.println("Original String is: "+s1);
+		
+		s1=s1.concat(" Learning");
+		
+		System.out.println("After concatenation String is: "+s1);
+		
+	}
+
+}
